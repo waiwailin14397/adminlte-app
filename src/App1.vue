63dc5bc149abe98f5611307bdec3c8a4a2f6f4ef
waiwailin14397 <waiwailin14397@gmail.com>
2023-12-05 17:27:09 +0900

    liff id

--- src/App1.vue
+++ src/App1.vue
@@ -9,13 +9,13 @@
 </template>
 
 <script>
-    import 'admin-lte/dist/css/adminlte.min.css';
-    import 'admin-lte/dist/js/adminlte.min.js';
-    import 'admin-lte/plugins/fontawesome-free/css/all.min.css';
+    // import 'admin-lte/dist/css/adminlte.min.css';
+    // import 'admin-lte/dist/js/adminlte.min.js';
+    // import 'admin-lte/plugins/fontawesome-free/css/all.min.css';
 
-    import Header from './components/Header.vue';
-    import Footer from './components/Footer.vue';
-    import Home from './views/Home.vue';
+    // import Header from './components/Header.vue';
+    // import Footer from './components/Footer.vue';
+    // import Home from './views/Home.vue';
 
     export default {
         // components: {
